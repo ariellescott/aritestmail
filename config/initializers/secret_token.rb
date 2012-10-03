127e3934fa10744f0e2a9b46028da5624e429e8f
arielle <ariellepatrice@gmail.com>
2012-10-03 13:19:23 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aritestmail::Application.config.secret_token = '6c8278911064dfb0dbdd1d54bcd7de9ef3e7993c284a3c307e36102a1f1421e6bb998eac4bb62511681a5f9fae7df805be3d87a6691a6ae338c84b4d7c0313e0'
